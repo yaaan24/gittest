adc397ed6d0136432fc0894ed8fbab4d0ab38c62
insuh <insuh@DESKTOP-GBEERHA>
2021-04-27 18:03:47 +0900

    날짜 출력문 추가 -인서

--- gittest/src/com/git/main/Main.java
+++ gittest/src/com/git/main/Main.java
@@ -1,9 +1,14 @@
 package com.git.main;
 
+import java.util.Date;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("¾È³ç git");
+		
+		Date today=new Date();
+		System.out.println(today);
 		
 	}
 

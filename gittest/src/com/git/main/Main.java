49ee0d5c8b64c78292041734c1c4df55743050a4
insuh <insuh@DESKTOP-GBEERHA>
2021-04-27 19:25:41 +0900

    #01 main에 출력문 추가 =bs

--- gittest/src/com/git/main/Main.java
+++ gittest/src/com/git/main/Main.java
@@ -5,10 +5,11 @@ import java.util.Date;
 public class Main {
 	
 	public static void main(String[] args) {
+		System.out.println("¾È");
 		System.out.println("¾È³ç git");
 		
 		Date today=new Date();
-		System.out.println(today);
+		System.out.println(today); 
 		
 	}
 

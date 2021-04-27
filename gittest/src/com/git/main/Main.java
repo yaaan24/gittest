3bdf23f44b97e357ad99f5f44d640ff95b199448
insuh <insuh@DESKTOP-GBEERHA>
2021-04-27 18:11:41 +0900

    #5 첨부터 다시

--- gittest/src/com/git/main/Main.java
+++ gittest/src/com/git/main/Main.java
@@ -1,15 +1,5 @@
 package com.git.main;
 
-import java.util.Date;
-
 public class Main {
-	
-	public static void main(String[] args) {
-		System.out.println("¾È³ç git");
-		
-		Date today=new Date();
-		System.out.println(today);
-		
-	}
 
 }
